0f0dae01e538ea2de8ec3b1ddc0e12994a24cda1
unknown <trista.li@wemoscooter.com>
2021-04-08 16:04:50 +0800

    Updates

---
+++ precache-manifest.27dee44aaf57aaeff1196e145b2b833a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dea1fab18e6fbef47173722f88f2d00",
+    "url": "/Heros/index.html"
+  },
+  {
+    "revision": "70ed90768c9514f29f87",
+    "url": "/Heros/static/js/2.5e1f8b7e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Heros/static/js/2.5e1f8b7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2949afd9db7361275c5a",
+    "url": "/Heros/static/js/main.64243489.chunk.js"
+  },
+  {
+    "revision": "f09850205e1ca75b371c",
+    "url": "/Heros/static/js/runtime-main.56705cb8.js"
+  },
+  {
+    "revision": "091b27fe7530607de29bd0666412befb",
+    "url": "/Heros/static/media/boomDialog.091b27fe.svg"
+  }
+]);
