c9be78b7d1e5eeb16725c23472c9a3daa44b7d33
unknown <trista.li@wemoscooter.com>
2021-04-12 11:52:32 +0800

    Corrected typos.

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Button from '../Button';
-import type { IconBottonType } from '../Button';
+import type { IconButtonType } from '../Button';
 
-function IconBotton(props: IconBottonType) {
+function IconButton(props: IconButtonType) {
   const { onClick } = props;
   return (
     <Button onClick={onClick} isIcon borderWidth='1px' {...props} />
   );
 }
 
-export default IconBotton;
+export default IconButton;
